35b37f24c2ec4088cdfa216dadf65650ec36254b
Shreesha <shreeshahrao1999@gmail.com>
2021-08-22 12:32:51 +0530

    My first project

--- First Project/ViewController.swift
+++ First Project/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var simpleText: UILabel!
+    
+    @IBOutlet weak var clickButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.blue
+        clickButton.backgroundColor = UIColor.yellow
     }
-
+    
+    
+    @IBAction func changeText(_ sender: UIButton) {
+        simpleText.text = "My First Project"
+        view.backgroundColor = UIColor.green
+        clickButton.backgroundColor = UIColor.red
+        
+    }
+    
 
 }
 
